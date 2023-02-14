d6aa15349c5ef94036e4865c2ca45d1d2fa89e77
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-02-14 14:57:04 -0300

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*/.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Oswald', ...fontFamily.sans]
+      },
       colors: {
-        fontFamily: {
-          sans: ['Oswald', ...fontFamily.sans]
-        },
         brand: {
           yellow: '#BCE784',
           orange: '#5DD39E',
