e02c04f134e27fbdd6ed801638748c23ce496e7b
Darkissons <inaciormgalvao@gmail.com>
2023-03-15 19:41:20 -0300

    feat: google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NBLL36Q');`}
+        </Script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
@@ -12,6 +21,15 @@ export default function Document() {
         />
       </Head>
       <body className="bg-slate-dark-1">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NBLL36Q"
+            height="0"
+            width="0"
+            className="hidden"
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
